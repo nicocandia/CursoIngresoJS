f409eb94bcc9e90303cef79b9a71421040097ab6
nicocandia <40811380+nicocandia@users.noreply.github.com>
2018-07-03 21:50:38 -0300

    primer guardado
    Son los ejercicios del 1 al 4.

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,23 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
+	//var person = prompt("Please enter your name", "Harry Potter");
+	//comentario de una linea
+	/*
+
+	esto es un comentario en bloque
+
+
+	*/
+	var nombre;
+	var nombre2;
+	//alert(nombre);
+	//nombre="pablo";
+	nombre=prompt("ingrese nombre","natalia natalia ");
 	
+	
+	alert("su nombre es: "+ nombre);
+    
+  
 }
 

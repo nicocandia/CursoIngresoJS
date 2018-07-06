9001effd102e9e8fd154746d805f438a7669c5c7
nicocandia <40811380+nicocandia@users.noreply.github.com>
2018-07-06 13:10:30 -0300

    Guardado ejercicio 6 entrada y salida
    Ejercicio 6

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,11 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-
+	var numerouno;
+	var numerodos;
+	numerouno=parseInt(document.getElementById('numeroUno').value);
+    numerodos=parseInt(document.getElementById('numeroDos').value);
+    var resultado=numerouno+numerodos;
+    alert("la suma es: "+resultado);
 }
 

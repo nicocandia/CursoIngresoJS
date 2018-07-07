6e741ed4263c27ddb22546bf1956e1a1beb7c33a
nicocandia <40811380+nicocandia@users.noreply.github.com>
2018-07-06 21:57:19 -0300

    Guardado ejercicios entrada y salida

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,11 +3,15 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	var numerouno;
-	var numerodos;
-	numerouno=parseInt(document.getElementById('numeroUno').value);
-    numerodos=parseInt(document.getElementById('numeroDos').value);
-    var resultado=numerouno+numerodos;
-    alert("la suma es: "+resultado);
-}
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroUno=parseInt(numeroUno);
+    numeroDos=document.getElementById('numeroDos').value;
+    numeroDos=parseInt(numeroDos);
+    resultado=numeroUno+numeroDos;
+    alert("el resultado es: "+ resultado);
+    }
 
+// primero declarar variables, despues cargarlas
--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,13 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	
+	var numeroDividendo;
+	var numeroDivisor;
+	var resultado;
+	numeroDividendo=document.getElementById('numeroDividendo').value;
+	numeroDividendo=parseInt(numeroDividendo);
+	numeroDivisor=document.getElementById('numeroDivisor').value;
+	numeroDivisor=parseInt(numeroDivisor);
+	resultado=numeroDividendo%numeroDivisor;
+	alert("el resto es: "+resultado);
 }

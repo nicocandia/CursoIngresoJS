24f79c36ccffb8781d3aa73a57e384362a192f4f
nicocandia <40811380+nicocandia@users.noreply.github.com>
2018-07-10 21:57:42 -0300

    Guardado ejercicios if
    Ejercicios del 1 al 6

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,13 @@
 function mostrar()
 {
 //tomo la edad  
+	var edad;
 
+	edad=document.getElementById('edad').value;
+
+	if (edad<12 || edad>17) 
+	{
+		alert("la persona no es adolescente");
+	}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,36 @@
 function mostrar()
 {
 //tomo la edad  
+/*	var edad;
 
+	edad=document.getElementById('edad').value;
 
+	if (edad>12 && edad<18) 
+	{
+		alert("la persona es adolescente");
+	}else
+	{	if(edad>17)
+		{
+			alert("la persona es mayor de edad")
+		}if(edad<13)
+		{
+			alert("la persona es un niño")
+		}
 
-
+	}
 }//FIN DE LA FUNCIÓN
+*/
+	if(edad<13)
+	{
+	alert("es menor"); 
+	}else{
+		if(edad>17)
+		{
+			alert("es mayor");
+		}
+		else
+		{
+			alert("es adolescente")
+		}
+	}
+}

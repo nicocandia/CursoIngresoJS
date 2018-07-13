368d643b17a7e1c728f8f88b7141d18d7b554ddf
nicocandia <40811380+nicocandia@users.noreply.github.com>
2018-07-13 13:30:54 -0300

    Guardado recuperatorio parcial
    Ejercicio 4

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,32 @@
 function mostrar()
 {
+	var numeroUno;
+	var numeroDos;
+	var suma;
+	var division;
 
+	numeroUno=prompt("ingrese primer numero");
+	numeroUno=parseInt(numeroUno);
+	numeroDos=prompt("ingrese numero dos");
+	numeroDos=parseInt(numeroDos);
+
+	if (numeroUno==numeroDos) 
+	{
+		alert(numeroUno+""+numeroDos)
+	}else
+	{
+		if(numeroUno>numeroDos)
+		{
+			division=numeroUno/numeroDos;
+			alert("division:"+ division);
+		}else
+		{
+			suma=numeroUno+numeroDos;
+			alert("suma:"+ suma);
+			if (suma<50) 
+			{
+				alert("la suma es: "+suma+" y es menor a 50");
+			}
+		}
+	}
 }

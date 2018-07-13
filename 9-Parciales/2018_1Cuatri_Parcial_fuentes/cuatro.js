eaeb92dbbb154486f5f92b59ecb4e879021cca9d
nicocandia <40811380+nicocandia@users.noreply.github.com>
2018-07-13 13:29:06 -0300

    Guardado parcial
    Ejercicio 4

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,34 @@
 function mostrar()
 {
+	var numeroUno;
+	var numeroDos;
+	var suma;
+	var resta;
 
+	numeroUno=prompt("ingrese primer numero");
+	numeroUno=parseInt(numeroUno);
+	numeroDos=prompt("ingrese numero dos");
+	numeroDos=parseInt(numeroDos);
+
+	if (numeroUno==numeroDos) 
+	{
+		alert(numeroUno+""+numeroDos)
+	}else
+	{
+		if(numeroUno>numeroDos)
+		{
+			resta=numeroUno-numeroDos;
+			alert("resta:"+ resta);
+		}else
+		{
+			suma=numeroUno+numeroDos;
+			alert("suma:"+ suma);
+			if (suma>10) 
+			{
+				alert("la suma es: "+suma+" y supero el 10");
+			}
+		}
+	}
 }
+
+

a188cd0ac38bd3038c49845cf58de2f21e8cf4d8
nicocandia <40811380+nicocandia@users.noreply.github.com>
2018-07-13 21:04:11 -0300

    Guardado ejercicios if
    Ejercicios 9 y 10

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,25 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var numerorandom;
+ 	numerorandom=Math.floor(Math.random()*10);  
+	numerorandom=parseInt(numerorandom);
 	
+	if (numerorandom==9 || numerorandom==10) 
+	{
+		alert("Excelente");
+	}
+	else
+	{
+		if(numerorandom>4)
+		{
+			alert("APROBO")
+		}
+		else
+		{
+			alert("Vamos la proxima se puede");
+		}
+	}
+
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,10 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var numerorandom;
+ 	numerorandom=Math.floor(Math.random()*10);  
+	numerorandom=parseInt(numerorandom);
+	alert(numerorandom);
+
 
 }//FIN DE LA FUNCIÓN

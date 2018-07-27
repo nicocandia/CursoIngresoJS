b43821189a34995f5b7263c23471974b3ece1e7f
nicocandia <40811380+nicocandia@users.noreply.github.com>
2018-07-27 10:39:33 -0300

    Guardado tp
    Ejercicio 12

--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -9,5 +9,51 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
  */
 function ComenzarIngreso () 
 {
- 
+	var edad;
+	var sexo;
+	var estadoCivil;
+	var sueldoBruto;
+	var numeroLegajo;
+	var nacionalidad;
+	
+	while(!(edad>17 && edad<91))
+	{
+		edad=prompt("ingrese edad");
+		edad=parseInt(edad);
+	}
+		document.getElementById('Edad').value=edad;
+
+	while(!(sexo=="M" || sexo=="F"))
+	{
+		sexo=prompt("ingrese sexo");
+	}
+		document.getElementById('Sexo').value=sexo;
+
+	while(!(estadoCivil==1 || estadoCivil==2 || estadoCivil==3 || estadoCivil==4))
+	{
+		estadoCivil=prompt("ingrese numero de estado civil");
+		estadoCivil=parseInt(estadoCivil);
+	}
+		document.getElementById('EstadoCivil').value=estadoCivil;
+
+	while(!(sueldoBruto>8000))
+	{
+		sueldoBruto=prompt("ingrese sueldo bruto");
+		sueldoBruto=parseInt(sueldoBruto);
+	}
+		document.getElementById('Sueldo').value=sueldoBruto;
+
+	while(!(numeroLegajo>999 && numeroLegajo<10000))
+	{
+		numeroLegajo=prompt("ingrese numero de legajo");
+		numeroLegajo=parseInt(numeroLegajo);
+	}
+		document.getElementById('Legajo').value=numeroLegajo;
+
+	while (!(nacionalidad=="A" || nacionalidad=="E" || nacionalidad=="N"))
+	{
+		nacionalidad=prompt("ingrese nacionalidad");
+	}
+		document.getElementById('Nacionalidad').value=nacionalidad;
 }
+
